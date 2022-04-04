284b1533b7b8080d70f0b0f4d01056a5073499d4
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-04-04 12:11:40 +0530

    Added socket.io and call events

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ app.get('/', (req, res) => {
   res.send('Web server is running fine.');
 });
 
+io.on('connection', (socket) => {
+  // emit me, that I joined.
+  socket.emit('me', socket.id);
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+    // forwarding the data we received.
+    io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+  });
+
+  socket.on('answerCall', (data) => {
+    // forwarding the data we received.
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
+
 server.listen(PORT, () => {
   console.log('Server is listening on PORT:', PORT);
 });
