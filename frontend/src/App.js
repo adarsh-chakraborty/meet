06c4f7f958203b0aa0d57ec0783ed93e269f4893
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-04-09 15:12:44 +0530

    Initial components setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,24 @@
+import { Typography, AppBar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Call
+        </Typography>
+      </AppBar>
+
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 }
 
 export default App;
