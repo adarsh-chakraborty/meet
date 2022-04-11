153a4904b4e92a70072d7e4a7617148a9f720f1d
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-04-11 12:40:44 +0530

    added styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,36 @@ import VideoPlayer from './components/VideoPlayer';
 import Options from './components/Options';
 import Notifications from './components/Notifications';
 
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%'
+    }
+  },
+  image: {
+    marginLeft: '15px'
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%'
+  }
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div>
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar position="static" color="inherit" className={classes.appBar}>
         <Typography variant="h2" align="center">
           Video Call
         </Typography>
