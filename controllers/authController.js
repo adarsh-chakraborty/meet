e77d5ef7f85a47740aeca4f42d22dda3b08c45e9
Adarsh Chakraborty <adarshchak108@gmail.com>
2022-05-12 19:55:52 +0530

    username available checking route

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,11 @@ const postRegister = async (req, res) => {
   }
 
   try {
+    const checkUser = await User.findOne({ username });
+    if (checkUser) {
+      return res.status(400).json({ error: 'Username already registered.' });
+    }
+
     const user = await User.create({
       username,
       email,
@@ -43,4 +48,29 @@ const postRegister = async (req, res) => {
     .json({ error: 'Error creating user, try again later.' });
 };
 
-module.exports = { getRefreshToken, postLogout, postLogin, postRegister };
+const getUsernameExistance = async (req, res) => {
+  const username = req.params.username;
+
+  if (username.length < 4) {
+    return res.json({
+      available: false,
+      message: 'Username should be atleast 4 chars long.'
+    });
+  }
+  const user = await User.findOne({ username });
+  if (user) {
+    return res.json({
+      available: false,
+      message: 'Username is taken, choose another.'
+    });
+  }
+  res.json({ available: true, message: 'Username is available' });
+};
+
+module.exports = {
+  getRefreshToken,
+  postLogout,
+  postLogin,
+  postRegister,
+  getUsernameExistance
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const Router = express.Router();
 
-Router.get('/refresh', authController.getRefreshToken);
+Router.get('/token', authController.getRefreshToken);
 Router.post('/register', authController.postRegister);
+Router.get('/user/:username', authController.getUsernameExistance);
 
 module.exports = Router;
